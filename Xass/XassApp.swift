b1936de2044aac9755285fb5c8a2b750b9b6351e
Ryan Dale <ryandale.apo@consulting-for.edenred.com>
2022-09-08 17:19:16 +0800

    Initial Commit

---
+++ Xass/XassApp.swift
@@ -0,0 +1,17 @@
+//
+//  XassApp.swift
+//  Xass
+//
+//  Created by Ryan Dale Apo on 9/8/22.
+//
+
+import SwiftUI
+
+@main
+struct XassApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
